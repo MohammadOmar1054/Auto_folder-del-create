6b6264efd81a51f26f52f32df1bd7f2d116ff1b8
Mohammed Omar <omarofficial1054@gmail.com>
2025-10-15 19:50:35 +0530

    Update file_crt.py

--- file_crt.py
+++ file_crt.py
@@ -1,22 +1,5 @@
-# import os 
-
-
-    
-
-# print("file we are working on : " , os.getcwd())
-
-
-# for i in range(10): 
-#     ex_fle = f"auto_created{i}"
-#     os_maker = os.system("mkdir auto_created{}".format(i))
-#     if os.path.exists(ex_fle): 
-#         print("file already exist in the system")
-
 import os
-
 print("file we are working on: ", os.getcwd())
-
-
 for i in range(4):
     folder_name = f"auto_created{i}"
     if os.path.exists(folder_name):
@@ -27,3 +10,4 @@ for i in range(4):
             print(f"Folder '{folder_name}' created successfully.")
         except:
             print(f"An unexpected error occurred while creating {folder_name}:")
+
