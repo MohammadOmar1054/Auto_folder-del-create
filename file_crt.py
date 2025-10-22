237ca0dcf4791d4801dc5e06dd266664fb581ac2
Mohammed Omar <omarofficial1054@gmail.com>
2025-10-22 21:31:45 +0530

    Update file_crt.py
    I deployed a Object Oriented Programming to make this even more useful to import in other files for further cases.

--- file_crt.py
+++ file_crt.py
@@ -1,15 +1,30 @@
-import os
-print("file we are working on: ", os.getcwd())
-for i in range(4):
-    folder_name = f"auto_created{i}"
-    #change auto_created to the name you wish to give to the folder
-    if os.path.exists(folder_name):
-        print(f"Folder '{folder_name}' already exists!")
-    else:
-        try:
-            os.mkdir(folder_name)
-            print(f"Folder '{folder_name}' created successfully.")
-        except:
-            print(f"An unexpected error occurred while creating {folder_name}:")
+import os 
 
+class folder_create:
+    print("File we are working on : ", os.getcwd())
+    def __init__(self, folder_name): 
+        self.folder_name = folder_name
+        
+    def create(self): 
+        for i in range(10): 
+            if os.path.exists(self.folder_name):
+                print(f"Folder '{self.folder_name}' already exists!")
+                
+            else :
+                try:
+                    os.mkdir(self.folder_name)
+                    print('creating file.......')
+                    print(f"Folder '{self.folder_name}' created successfully.")
+                except:
+                    print(f"An unexpected error occurred while creating {self.folder_name}:")
 
+            ask_more = str(input('Do you wish to create more (y/n) : ')) 
+            if ask_more=="y":
+                self.foler_name = str(input('Enter the new folder name : '))
+                
+            elif ask_more=="n": 
+                break 
+if __name__ ==  "__main__":          
+    nm_fl= str(input("Enter the name of the folder : "))
+    folder = folder_create(nm_fl)
+    folder.create()
